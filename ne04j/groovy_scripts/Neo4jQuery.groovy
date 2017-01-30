a9aa82c33aa4851589c12f58144ff131227a7267
Yashwant Keswani <yashwant.keswani@gmail.com>
2017-01-30 19:02:07 +0530

    Few Subtle Changes

--- ne04j/groovy_scripts/Neo4jQuery.groovy
+++ ne04j/groovy_scripts/Neo4jQuery.groovy
@@ -11,14 +11,14 @@ println "==============Starting to Run The Queries=========="
 for (i in 1..no_of_times) {
 	println "################Run "+i+"##################"
 	"sync".execute()
-        "echo 3 > /proc/sys/vm/drop_caches"
+        "echo 3 > /proc/sys/vm/drop_caches".execute()
 	println "======Query 1======"
 	s = System.currentTimeMillis();
 	x.V.count()
 	println "Time taken for Query 1:" + (System.currentTimeMillis() - s)
 	
 	"sync".execute().text
-	"echo 3 > /proc/sys/vm/drop_caches"
+	"echo 3 > /proc/sys/vm/drop_caches".execute()
 	println "======Query 2======"
 	s = System.currentTimeMillis();
 	x.E.count()
